ac3b7b6ff4ca523539c71bd33ac42157dc439d6b
Anderskruse <32634165+anderskruse@users.noreply.github.com>
2019-05-06 12:04:02 +0200

    normalized positions and player box collider :)

---
+++ Infinity Runner/Assets/Scripts/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        player.transform.Translate(0f, 0f, 8f * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.transform.Translate(-8f * Time.deltaTime, 0f, 0f);
+
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            player.transform.Translate(8f * Time.deltaTime, 0f, 0f);
+
+        }
+
+    }
+}
---
+++ Infinity Runner/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject cam;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = cam.transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        cam.transform.position = player.transform.position + offset;
+    }
+
+    void updatePlayerObject()
+    {
+
+    }
+}
+
