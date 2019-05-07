ae86892b6fa0ef37d5a90c869597b2c246058f67
EmilVHermansen <emil.valbak.hermansen@gmail.com>
2019-05-07 13:08:53 +0200

    added angle change on movement and refactored

--- Infinity Runner/Assets/Scripts/Movement.cs
+++ Infinity Runner/Assets/Scripts/Movement.cs
@@ -7,27 +7,53 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     public GameObject player;
 
+    //movements
+    Vector3 forward;
+    Vector3 left;
+    Vector3 right;
+
     void Start()
     {
+        forward = new Vector3(0f, 0f, 8f * Time.deltaTime);
+        left = new Vector3(-8f * Time.deltaTime, 0f, 0f);
+        right = new Vector3(8f * Time.deltaTime, 0f, 0f);
+
 
     }
 
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        player.transform.Translate(0f, 0f, 8f * Time.deltaTime);
 
+        MoveForward();
         if (Input.GetKey(KeyCode.A))
         {
-            player.transform.Translate(-8f * Time.deltaTime, 0f, 0f);
-
+            TurnLeft();
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            player.transform.Translate(8f * Time.deltaTime, 0f, 0f);
-
+            TurnRight();
         }
 
     }
+
+    private void MoveForward()
+    {
+        player.transform.Translate(forward);
+        player.transform.rotation = Quaternion.LookRotation(forward);
+    }
+
+    private void TurnLeft()
+    {
+        player.transform.Translate(left, Space.World);
+        player.transform.rotation = Quaternion.LookRotation(left + forward * 2);
+    }
+
+    private void TurnRight()
+    {
+        player.transform.Translate(right, Space.World);
+        player.transform.rotation = Quaternion.LookRotation(right + forward * 2);
+    }
+
 }
