799030f6c9dada244c3326b974f6a7966c7eacba
EmilVHermansen <emil.valbak.hermansen@gmail.com>
2019-05-14 12:25:54 +0200

    Fixed camera for penguin, added some diamonds (need pickup script)

--- Infinity Runner/Assets/Scripts/PlayerCamera.cs
+++ Infinity Runner/Assets/Scripts/PlayerCamera.cs
@@ -7,6 +7,7 @@ public class PlayerCamera : MonoBehaviour
     public GameObject player;
     //public Transform target;
     public GameObject cam;
+    Vector3 customOffset;
 
     private Vector3 offset;
 
@@ -19,13 +20,25 @@ public class PlayerCamera : MonoBehaviour
 
     void LateUpdate()
     {
-        cam.transform.position = player.transform.position + offset;
+        cam.transform.position = player.transform.position + offset + customOffset;
     }
 
 
     public void updatePlayerObject(GameObject player)
     {
         this.player = player;
+        switch (player.gameObject.name)
+        {
+            case "Penguin":
+                Debug.Log("Went in here");
+                customOffset = new Vector3(0f, -1f, 0f);
+                break;
+
+            default:
+                customOffset = new Vector3(0f, 0f, 0f);
+                break;
+        }
+
     }
 }
 
